2fd5048e86187b66c6900a8c8b39d84d83842b59
Durfan <cecilio@aluno.ufsj.edu.br>
2022-04-24 04:40:13 -0300

    pylint sucks

--- app/flask_app.py
+++ app/flask_app.py
@@ -3,7 +3,7 @@ import re
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-with open(f'{app.root_path}/data/data.json', "r") as jfile:
+with open(f'{app.root_path}/data/data.json', 'r', encoding='utf-8') as jfile:
     data = json.load(jfile)
 grammar = data['grammar']
 tokenType = data['tokenType']
@@ -14,37 +14,43 @@ final = data['final']
 
 @app.context_processor
 def inject_grammar():
+    '''Insere grammar nos templates'''
     dump_grammar = json.dumps(grammar, indent=5, separators=(",", "\t:"))
     return dict(grammar=dump_grammar, final=final)
 
 
 @app.route("/")
 def landing():
+    '''Renderiza a landing page'''
     return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def analysis_process():
+    '''Renderiza a pagina com os resultados'''
     code = request.form['code']
     out, path = automato(code)
     dump = json.dumps(out, indent=2)
     return render_template('index.html', path=path, output=out, jsonDump=dump)
 
 
-def get_transition(c):
+def get_transition(char):
+    '''Retorna a coluna na tabela de transicao'''
     for key, _unused in grammar.items():
-        if re.match(key, c):
+        if re.match(key, char):
             return list(grammar.keys()).index(key)
     return None
 
 
 def get_type(token, state):
+    '''Retorna se o token possui o id reservado'''
     if token in reserved:
         return 'RESERVADO'
     return tokenType[str(state)]
 
 
 def automato(code):
+    '''Executa a analise por meio do DFA'''
     out = []
     path = []
     state = 0
