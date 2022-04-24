7d4c05c2eb886b6fb69e8c786cf166a5dcf8330a
Durfan <cecilio@aluno.ufsj.edu.br>
2022-04-24 03:25:38 -0300

    fix styles

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,4 +1,5 @@
-import json, re
+import json
+import re
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
@@ -16,9 +17,11 @@ def inject_grammar():
     dump_grammar = json.dumps(grammar, indent=5, separators=(",","\t:"))
     return dict(grammar=dump_grammar, final=final)
 
+
 @app.route("/")
 def landing():
     return render_template('index.html')
+
 
 @app.route('/', methods=['POST'])
 def analysis_process():
@@ -27,16 +30,19 @@ def analysis_process():
     dump = json.dumps(out, indent=2)
     return render_template('index.html', path=path, output=out, jsonDump=dump)
 
+
 def get_transition(c):
     for key,value in grammar.items():
         if re.match(key, c):
             return list(grammar.keys()).index(key)
     return None
 
+
 def get_type(token, state):
     if token in reserved:
         return 'RESERVADO'
     return tokenType[str(state)]
+
 
 def automato(input):
     out = []
@@ -51,7 +57,7 @@ def automato(input):
         c = input[i]
         path.append(state)
         if c == '\n':
-            line+=1
+            line += 1
 
         token += c
         tr = get_transition(c)
@@ -61,15 +67,15 @@ def automato(input):
             idx = i
             path.append(state)
             if state in retract:
-                i-=1
+                i -= 1
                 token = token[:-1].strip()
                 idx = (i+1)-len(token)
-            type = get_type(token,state)
+            type = get_type(token, state)
             pos = {'line': line, 'idx': idx}
-            out.append({'pos':pos, 'state':state, 'class':type, 'value':token})
+            out.append({'pos': pos, 'state': state, 'class': type, 'value': token})
             state = 0
             token = ''
 
-        i+=1
+        i += 1
 
     return out, path
